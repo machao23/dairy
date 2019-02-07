62658aa88316fa0bd6e271af1db684ca6b840932
ma_chao <ma_chao@Ctrip.com>
2019-02-07 13:24:40 +0800

    合并netty

--- netty/NettyTcpServer.java
+++ netty/NettyTcpServer.java
@@ -53,14 +53,18 @@ public class NettyTcpServer extends AbstractServer {
 }
 
 public class ServerBootstrap extends AbstractBootstrap<ServerBootstrap, ServerChannel> {
+
+	// childHandler就是ServerBootstrap在build的时候指定处理客户端请求的handler
+	private final ChannelHandler childHandler;
+
 	// 客户端连接进来会调用channelRead
 	@Override
 	@SuppressWarnings("unchecked")
 	public void channelRead(ChannelHandlerContext ctx, Object msg) {
-		// child就是客户端的channel
+		// child就是客户端的channel，来自通用的msg入参
 		final Channel child = (Channel) msg;
 
-		// childHandler就是ServerBootstrap在build的时候指定处理客户端请求的handler
+		// 添加handler
 		child.pipeline().addLast(childHandler);
 
 		// childGroup即workerGroup
@@ -110,138 +114,4 @@ public class NioServerSocketChannel extends AbstractNioMessageChannel
 		return 1;
     }
 
-}
-
-// Netty 中对本地线程的抽象,SingleThreadEventExecutor的父类
-public abstract class SingleThreadEventExecutor extends AbstractScheduledEventExecutor implements OrderedEventExecutor {
-	// 本质是这个thread
-	private volatile Thread thread;
-
-	// 定时task的队列在父类里实现
-	Queue<ScheduledFutureTask<?>> scheduledTaskQueue;
-
-	@Override
-    public void execute(Runnable task) {
-        boolean inEventLoop = inEventLoop();
-        if (inEventLoop) {
-            addTask(task);
-        } else {
-			// 主线程进来的还得先启动eventLoop线程
-            startThread();
-            addTask(task);
-        }
-    }
-}
-
-public final class NioEventLoop extends SingleThreadEventLoop {
-	@Override
-    protected void run() {
-		// 事件无限循环
-        for (;;) {
-			// hasTasks查看taskQueue队列里是否任务,调用非阻塞selector.selectNow迅速拿到就绪IO集合,selector.wakeup唤醒被select阻塞的线程,然后走到default分支，
-			// 没有task就返回SelectStrategy.SELECT继续阻塞等待
-			switch (selectStrategy.calculateStrategy(selectNowSupplier, hasTasks())) {
-				case SelectStrategy.CONTINUE:
-					continue;
-				case SelectStrategy.SELECT:
-					select(wakenUp.getAndSet(false));
-					if (wakenUp.get()) {
-						selector.wakeup();
-					}
-					// fall through
-				default:
-			}
-
-			// ioRatio表示这个thread分配给io和执行task的时间比
-			final int ioRatio = this.ioRatio;
-			if (ioRatio == 100) {
-				try {
-					processSelectedKeys();
-				} finally {
-					runAllTasks();
-				}
-			} else {
-				final long ioStartTime = System.nanoTime();
-				try {
-					// 实质会走到processSelectedKey
-					processSelectedKeys();
-				} finally {
-					final long ioTime = System.nanoTime() - ioStartTime;
-					runAllTasks(ioTime * (100 - ioRatio) / ioRatio);
-				}
-			}
-        }
-    }
-
-	// 调用NIO的selecor的非阻塞select
-	int selectNow() throws IOException {
-        try {
-            return selector.selectNow();
-        } finally {
-            // restore wakeup state if needed
-            if (wakenUp.get()) {
-                selector.wakeup();
-            }
-        }
-    }
-
-	// NIO处理selector就绪的流程
-	private void processSelectedKey(SelectionKey k, AbstractNioChannel ch) {
-        final AbstractNioChannel.NioUnsafe unsafe = ch.unsafe();
-
-		int readyOps = k.readyOps();
-		// 连接建立
-		if ((readyOps & SelectionKey.OP_CONNECT) != 0) {
-			int ops = k.interestOps();
-			// 需要将 OP_CONNECT 从就绪事件集中清除, 不然会一直有 OP_CONNECT 事件.
-			ops &= ~SelectionKey.OP_CONNECT;
-			k.interestOps(ops);
-			// unsafe.finishConnect() 调用最后会调用到 pipeline().fireChannelActive(), 产生一个 inbound 事件, 通知 pipeline 中的各个 handler TCP 通道已建立
-			unsafe.finishConnect();
-		}
-
-		// 可写
-		if ((readyOps & SelectionKey.OP_WRITE) != 0) {
-			ch.unsafe().forceFlush();
-		}
-
-		// 可读
-		if ((readyOps & (SelectionKey.OP_READ | SelectionKey.OP_ACCEPT)) != 0 || readyOps == 0) {
-			unsafe.read();
-		}
-    }
-}
-
-protected class NioByteUnsafe extends AbstractNioUnsafe {
-	// OP_READ触发读取数据
-	@Override
-	public final void read() {
-		try {
-			do {
-				byteBuf = allocHandle.allocate(allocator);
-				allocHandle.lastBytesRead(doReadBytes(byteBuf));
-				if (allocHandle.lastBytesRead() <= 0) {
-					// nothing was read. release the buffer.
-					byteBuf.release();
-					byteBuf = null;
-					close = allocHandle.lastBytesRead() < 0;
-					break;
-				}
-
-				allocHandle.incMessagesRead(1);
-				readPending = false;
-				// 触发inbound的起点事件
-				pipeline.fireChannelRead(byteBuf);
-				byteBuf = null;
-			} while (allocHandle.continueReading());
-
-			allocHandle.readComplete();
-			pipeline.fireChannelReadComplete();
-
-		} finally {
-			if (!readPending && !config.isAutoRead()) {
-				removeReadOp();
-			}
-		}
-    }
 }
