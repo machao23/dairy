93e5824133f6175112a458da278684c4d98ae746
ma_chao <ma_chao@Ctrip.com>
2019-08-22 18:32:51 +0800

    猪八戒

--- jdk/Allocate.cpp
+++ jdk/Allocate.cpp
@@ -32,22 +32,41 @@ IRT_END
 
 // JVM会先创建instanceklass，然后通过allocate_instance方法分配一个instanceOop
 instanceOop InstanceKlass::allocate_instance(TRAPS) {
+  //是否重写finalize()方法
+  bool has_finalizer_flag = has_finalizer(); // Query before possible GC
+  //分配的对象的大小
   int size = size_helper();  // Query before forming handle.
-
   KlassHandle h_k(THREAD, this);
 
   instanceOop i;
   // 在堆内存上进行分配
   i = (instanceOop)CollectedHeap::obj_allocate(h_k, size, CHECK_NULL);
+  // 如果之前判断对象重写了finalize()方法，将该对象注册到 Finalizer 队列里面
+  if (has_finalizer_flag && !RegisterFinalizersAtInit) {
+    i = register_finalizer(i, CHECK_NULL);
+  }
   return i;
 }
 
 oop CollectedHeap::obj_allocate(KlassHandle klass, int size, TRAPS) {
-  // 实质是调用 common_mem_allocate_noinit
+  //校验在GC的时候不分配内存
+  assert(!Universe::heap()->is_gc_active(), "Allocation during gc not allowed");
+  //分配大小大于0
+  assert(size >= 0, "int won't convert to size_t");
+  // 内存分配，实质是调用 common_mem_allocate_noinit
   HeapWord* obj = common_mem_allocate_init(klass, size, CHECK_NULL);
+  // 初始化
   post_allocation_setup_obj(klass, obj, size);
   NOT_PRODUCT(Universe::heap()->check_for_bad_heap_word_value(obj, size));
   return (oop)obj;
+}
+
+HeapWord* CollectedHeap::common_mem_allocate_init(KlassHandle klass, size_t size, TRAPS) {
+  //申请内存
+  HeapWord* obj = common_mem_allocate_noinit(klass, size, CHECK_NULL);
+  //字节填充对齐
+  init_obj(obj, size);
+  return obj;
 }
 
 HeapWord* CollectedHeap::common_mem_allocate_noinit(KlassHandle klass, size_t size, TRAPS) {
@@ -62,7 +81,7 @@ HeapWord* CollectedHeap::common_mem_allocate_noinit(KlassHandle klass, size_t si
   }
 
   bool gc_overhead_limit_was_exceeded = false;
-  // 快速分配失败后慢速分配
+  // 快速分配失败后慢速在堆中分配
   result = Universe::heap()->mem_allocate(size,
                                           &gc_overhead_limit_was_exceeded);
   if (result != NULL) {
@@ -114,7 +133,7 @@ HeapWord* CollectedHeap::allocate_from_tlab(KlassHandle klass, Thread* thread, s
     return obj;
   }
   // Otherwise...
-  // 分配失败
+  // 分配失败就慢分配
   return allocate_from_tlab_slow(klass, thread, size);
 }
 
@@ -256,10 +275,11 @@ inline HeapWord* G1CollectedHeap::attempt_allocation(size_t word_size,
                                                      uint* gclocker_retry_count_ret) {
   AllocationContext_t context = AllocationContext::current();
   // 快速无锁分配
+  //_mutator_alloc_region内部持有一个引用_alloc_region，指向当前正活跃的eden region
   HeapWord* result = _allocator->mutator_alloc_region(context)->attempt_allocation(word_size,
                                                                                    false /* bot_updates */);
   if (result == NULL) {
-  	// 慢速加锁分配
+  	//通过CAS分配对象失败，通过慢速加锁分配内存
     result = attempt_allocation_slow(word_size,
                                      context,
                                      gc_count_before_ret,
@@ -270,6 +290,190 @@ inline HeapWord* G1CollectedHeap::attempt_allocation(size_t word_size,
     dirty_young_block(result, word_size);
   }
   return result;
+}
+
+inline HeapWord* G1AllocRegion::attempt_allocation(size_t word_size,
+                                                   bool bot_updates) {
+
+  //获得当前活跃的region
+  // _alloc_region 指针，该指针执行当前活跃的 region
+  HeapRegion* alloc_region = _alloc_region;
+  
+  // 调用 par_allocate 方法，分配TLAB空间， par_allocate 最终调用 par_allocate_impl 方法
+  HeapWord* result = par_allocate(alloc_region, word_size, bot_updates);
+  if (result != NULL) {
+    trace("alloc", word_size, result);
+    return result;
+  }
+  trace("alloc failed", word_size);
+  return NULL;
+}
+
+inline HeapWord* ContiguousSpace::par_allocate_impl(size_t size,
+                                                    HeapWord* const end_value) {
+  do {
+    HeapWord* obj = top();
+    if (pointer_delta(end_value, obj) >= size) {
+      HeapWord* new_top = obj + size;
+      //采用指针碰撞方式，分配对象内存（CAS）
+      HeapWord* result = (HeapWord*)Atomic::cmpxchg_ptr(new_top, top_addr(), obj);
+      // result can be one of two:
+      //  the old top value: the exchange succeeded
+      //  otherwise: the new value of the top is returned.
+      if (result == obj) {
+        assert(is_aligned(obj) && is_aligned(new_top), "checking alignment");
+        return obj;
+      }
+    } else {
+      return NULL;
+    }
+  } while (true);
+}
+
+// 有锁慢申请TLAB
+HeapWord* G1CollectedHeap::attempt_allocation_slow(size_t word_size,
+                                           unsigned int *gc_count_before_ret) {
+  HeapWord* result = NULL;
+  for (int try_count = 1; /* we'll return */; try_count += 1) {
+    bool should_try_gc;
+    unsigned int gc_count_before;
+    {
+      MutexLockerEx x(Heap_lock);//获取堆的锁
+	  // 尝试分配内存
+      result = _mutator_alloc_region.attempt_allocation_locked(word_size,
+                                                      false /* bot_updates */);
+      if (result != NULL) {
+        return result;
+      }
+      //如果执行到这里，说明内存分配失败
+      assert(_mutator_alloc_region.get() == NULL, "only way to get here");
+
+      if (GC_locker::is_active_and_needs_gc()) {//当前是JNI方法调用，执行扩容操作】
+        //如果还有空闲的区域
+        if (g1_policy()->can_expand_young_list()) {
+          // No need for an ergo verbose message here,
+          // can_expand_young_list() does this when it returns true.
+          result = _mutator_alloc_region.attempt_allocation_force(word_size,
+                                                      false /* bot_updates */);
+          if (result != NULL) {
+            return result;
+          }
+        }
+        should_try_gc = false;
+      } else {
+        if (GC_locker::needs_gc()) {
+          should_try_gc = false;
+        } else {
+          // Read the GC count while still holding the Heap_lock.
+          gc_count_before = total_collections();
+          should_try_gc = true;
+        }
+      }
+    }
+
+    if (should_try_gc) {
+      bool succeeded;
+      //触发GC
+      result = do_collection_pause(word_size, gc_count_before, &succeeded);
+      if (result != NULL) {
+        assert(succeeded, "only way to get back a non-NULL result");
+        return result;
+      }
+
+    result = _mutator_alloc_region.attempt_allocation(word_size,
+                                                      false /* bot_updates */);
+    if (result != NULL) {
+      return result;
+    }
+}
+
+// 有锁分配内存
+inline HeapWord* G1AllocRegion::attempt_allocation_locked(size_t word_size,
+                                                          bool bot_updates) {
+  // First we have to tedo the allocation, assuming we're holding the
+  // appropriate lock, in case another thread changed the region while
+  // we were waiting to get the lock.
+  // 本质和无锁一样，调用 attempt_allocation 分配内存
+  HeapWord* result = attempt_allocation(word_size, bot_updates);
+  if (result != NULL) {
+    return result;
+  }
+
+  retire(true /* fill_up */);
+  result = new_alloc_region_and_allocate(word_size, false /* force */);
+  if (result != NULL) {
+    trace("alloc locked (second attempt)", word_size, result);
+    return result;
+  }
+  trace("alloc locked failed", word_size);
+  return NULL;
+}
+
+HeapWord* G1AllocRegion::new_alloc_region_and_allocate(size_t word_size,
+
+  //分配一个新的region
+  HeapRegion* new_alloc_region = allocate_new_region(word_size, force);
+  if (new_alloc_region != NULL) {
+    new_alloc_region->reset_pre_dummy_top();
+    // Need to do this before the allocation
+    _used_bytes_before = new_alloc_region->used();
+    HeapWord* result = allocate(new_alloc_region, word_size, _bot_updates);
+    assert(result != NULL, ar_ext_msg(this, "the allocation should succeeded"));
+
+    OrderAccess::storestore();
+    _alloc_region = new_alloc_region;
+    _count += 1;
+    trace("region allocation successful");
+    return result;
+  } else {
+    trace("region allocation failed");
+    return NULL;
+  }
+}
+
+// 分配新的region
+HeapRegion* G1CollectedHeap::new_mutator_alloc_region(size_t word_size,
+                                                      bool force) {
+  //判断当前young_list中的region数是否已经超过阈值
+  bool young_list_full = g1_policy()->is_young_list_full();
+  if (force || !young_list_full) {
+	// 新分配一个region
+    HeapRegion* new_alloc_region = new_region(word_size,
+                                              false /* do_expand */);
+    if (new_alloc_region != NULL) {
+      set_region_short_lived_locked(new_alloc_region);
+      _hr_printer.alloc(new_alloc_region, G1HRPrinter::Eden, young_list_full);
+      return new_alloc_region;
+    }
+  }
+  return NULL;
+}
+
+// TLAB分配失败，在Eden上分配内存
+HeapWord* G1CollectedHeap::mem_allocate(size_t word_size,
+                              bool*  gc_overhead_limit_was_exceeded) {
+  assert_heap_not_locked_and_not_at_safepoint();
+
+  for (int try_count = 1; /* we'll return */; try_count += 1) {
+    unsigned int gc_count_before;
+
+    HeapWord* result = NULL;
+    if (!isHumongous(word_size)) {
+      //在Region里面分配对象
+      result = attempt_allocation(word_size, &gc_count_before);
+    } else {
+     //在region中分配大对象
+      result = attempt_allocation_humongous(word_size, &gc_count_before);
+    }
+    if (result != NULL) {
+      return result;
+    }
+
+    // 触发GC
+    VM_G1CollectForAllocation op(gc_count_before, word_size);
+    // ...and get the VM thread to execute it.
+    VMThread::execute(&op);
+    ..........................省略.............................
 }
 
 void ThreadLocalAllocBuffer::fill(HeapWord* start,
