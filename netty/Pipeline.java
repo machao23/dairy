3e91427a220baad03d5adcc2fc2921f6bfdc85d3
ma_chao <ma_chao@Ctrip.com>
2019-09-12 19:40:26 +0800

    channel hander

--- netty/Pipeline.java
+++ netty/Pipeline.java
@@ -1,5 +1,60 @@
-abstract class AbstractChannelHandlerContext extends DefaultAttributeMap implements ChannelHandlerContext, ResourceLeakHint {
+// 默认的pipeline
+public class DefaultChannelPipeline implements ChannelPipeline {
+	// pipeline 中的节点的数据结构是 ChannelHandlerContext 类。
+	// 每个 ChannelHandlerContext 包含一个 ChannelHandler、它的上下节点( 从而形成 ChannelHandler 链 )、以及其他上下文。
+	final AbstractChannelHandlerContext head;
+    final AbstractChannelHandlerContext tail;
+	
+	protected DefaultChannelPipeline(Channel channel) {
+		// head 节点向下指向 tail 节点，tail 节点向上指向 head 节点，从而形成相互的指向
+		// 默认情况下，pipeline 有 head 和 tail 节点，形成默认的 ChannelHandler 链。
+		// 而我们可以在它们之间，加入自定义的 ChannelHandler 节点。
+        tail = new TailContext(this);
+        head = new HeadContext(this);
 
+        head.next = tail;
+        tail.prev = head;
+    }
+	
+	final class HeadContext extends AbstractChannelHandlerContext
+            implements ChannelOutboundHandler, ChannelInboundHandler {
+
+        private final Unsafe unsafe;
+
+        HeadContext(DefaultChannelPipeline pipeline) {
+			// 调用父 AbstractChannelHandlerContext 的构造方法，设置 inbound = false、outbound = true 
+            super(pipeline, null, HEAD_NAME, true, true);
+			// 使用 Channel 的 Unsafe 作为 unsafe 属性。
+			// HeadContext 实现 ChannelOutboundHandler 接口的方法，都会调用 Unsafe 对应的方法。
+            unsafe = pipeline.channel().unsafe();
+            setAddComplete();
+        }
+		
+		// 返回自己作为 Context 的 ChannelHandler 。
+		// 因为 HeadContext ，实现 ChannelOutboundHandler、ChannelInboundHandler 接口，而它们本身就是 ChannelHandler 。
+		@Override
+		public ChannelHandler handler() {
+			return this;
+		}
+	}
+}
+
+final class DefaultChannelHandlerContext extends AbstractChannelHandlerContext {
+	// 不同于 HeadContext、TailContext，它们自身就是一个 Context 的同时，也是一个 ChannelHandler 。
+	// 而 DefaultChannelHandlerContext 是内嵌 一个 ChannelHandler
+    private final ChannelHandler handler;
+}
+
+abstract class AbstractChannelHandlerContext extends DefaultAttributeMap implements ChannelHandlerContext, ResourceLeakHint {
+	// 下一个context节点
+	volatile AbstractChannelHandlerContext next;
+	// 上一个context节点
+    volatile AbstractChannelHandlerContext prev;
+	/**
+	 * 所属 pipeline
+	 */
+	private final DefaultChannelPipeline pipeline;
+	
 	// pipeline写数据时调用tailContext时被调用
 	@Override
     public ChannelFuture write(Object msg) {
