8e529a79944d3eb1492bea8a4aef2f065811ec79
ma_chao <ma_chao@Ctrip.com>
2020-01-24 15:59:06 +0800

    decision trees

--- machine/trees.py
+++ machine/trees.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*
+'''
+Created on Oct 12, 2010
+Decision Tree Source Code for Machine Learning in Action Ch. 3
+@author: Peter Harrington
+'''
 from math import log
-# 初始化测试样本数据，2个特征+结果
+import operator
+
 def createDataSet():
     dataSet = [[1, 1, 'yes'],
                [1, 1, 'yes'],
@@ -10,53 +17,68 @@ def createDataSet():
     #change to discrete values
     return dataSet, labels
 
-# 计算每组样本的香农熵，即样本的纯度
+# 计算样本集合的香农熵
 def calcShannonEnt(dataSet):
     numEntries = len(dataSet)
     labelCounts = {}
+	# 对分类分组计数
     for featVec in dataSet: #the the number of unique elements and their occurance
         currentLabel = featVec[-1]
         if currentLabel not in labelCounts.keys(): labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
     shannonEnt = 0.0
     for key in labelCounts:
+		# prob就是某个分类占总量的比例，即发生的概率
         prob = float(labelCounts[key])/numEntries
+		# 以2为底求对数获取，然后累加
+		# 得到所有类别所有可能值包含的信息期望值，即香农熵
         shannonEnt -= prob * log(prob,2) #log base 2
     return shannonEnt
 
-# 把样本集dataSet按照指定特征的下标axis，筛选出指定值value的子数据集
+# dataSet: 待划分的数据集
+# axis: 划分数据集的特征下标，即第几个特征
+# 提取 axis下标的feature值 == value 的样本，并且样本集结果里去掉了该下标feature
 def splitDataSet(dataSet, axis, value):
     retDataSet = []
     for featVec in dataSet:
+		# 只有指定特征 == 指定的特征值，才返回
         if featVec[axis] == value:
+			# 这里是去掉样本里的指定特征:
+			# 先截取从开头到指定样本下标前之间的片段1
             reducedFeatVec = featVec[:axis]     #chop out axis used for splitting
+			# 再截取从指定样本下标后到末尾之间的片段2，把片段1和片段2
             reducedFeatVec.extend(featVec[axis+1:])
             retDataSet.append(reducedFeatVec)
     return retDataSet
-
+    
+# 选择最好的数据集划分方式
 def chooseBestFeatureToSplit(dataSet):
-	# 特征数量，按特征维度轮询计算熵
+	# 所有特征的种数
     numFeatures = len(dataSet[0]) - 1      #the last column is used for the labels
+	# 这个是最原始的未划分的基础香农熵，用来比较计算收益的
     baseEntropy = calcShannonEnt(dataSet)
     bestInfoGain = 0.0; bestFeature = -1
+	# 穷尽遍历所有特征
     for i in range(numFeatures):        #iterate over all the features
+		# featList: 获取这个特征的值集合
         featList = [example[i] for example in dataSet]#create a list of all the examples of this feature
-		# 获取某个特征下所有的枚举值
+		# 去重
         uniqueVals = set(featList)       #get a set of unique values
         newEntropy = 0.0
-		# 遍历枚举值，按枚举值对数据集做分组，计算累计熵
+		# 穷尽遍历这个特征的所有值划分集合，计算每次
         for value in uniqueVals:
             subDataSet = splitDataSet(dataSet, i, value)
             prob = len(subDataSet)/float(len(dataSet))
-            newEntropy += prob * calcShannonEnt(subDataSet)
+            newEntropy += prob * calcShannonEnt(subDataSet)     
+		# 计算这次划分前后的收益
         infoGain = baseEntropy - newEntropy     #calculate the info gain; ie reduction in entropy
         if (infoGain > bestInfoGain):       #compare this to the best gain so far
             bestInfoGain = infoGain         #if better than current best, set to best
+			# 记录最佳收益划分的特征
             bestFeature = i
-	# 返回按哪个特征分组能获得最优熵的结果
     return bestFeature                      #returns an integer
 
-# 所有特征都无法确定分类情况下，使用投票决策
+# 如果穷举了所有feature后，分组的结果里label不是唯一的，通常采用多数表决决定叶子节点属于哪一个label
 def majorityCnt(classList):
     classCount={}
     for vote in classList:
@@ -66,48 +88,37 @@ def majorityCnt(classList):
     return sortedClassCount[0][0]
 
 # 创建决策树
+# labels就是每个特征下标对应的字面意思
 def createTree(dataSet,labels):
-    # classList就是所有的分类结果集合
     classList = [example[-1] for example in dataSet]
-    # 如果遍历到了具体一个分类结果，就直接返回
-    if classList.count(classList[0]) == len(classList):
+	# 分组后每组的label只有一个，停止分组
+    if classList.count(classList[0]) == len(classList): 
         return classList[0]#stop splitting when all of the classes are equal
-    # 所有特征都不能决定出唯一的分类结果，就使用投票策略
+	# 遍历完所有feature，停止分组
     if len(dataSet[0]) == 1: #stop splitting when there are no more features in dataSet
         return majorityCnt(classList)
-	# 根据香农熵找到最佳切分的起始特征
     bestFeat = chooseBestFeatureToSplit(dataSet)
-    # 根据下标找到特征的描述标签
     bestFeatLabel = labels[bestFeat]
-    # 该特征作为树的节点
     myTree = {bestFeatLabel:{}}
-	# 删除该特征，继续从其他特征决策树枝
     del(labels[bestFeat])
-	# 列出最优熵的特征的枚举值
     featValues = [example[bestFeat] for example in dataSet]
     uniqueVals = set(featValues)
-	# 根据枚举值开始创建该特征节点下的树枝或树叶
     for value in uniqueVals:
         subLabels = labels[:]       #copy all of labels, so trees don't mess up existing labels
+		# 递归对剩余的feature分组
         myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value),subLabels)
-    return myTree
-	
+    return myTree                            
+
+# 传入要预测的样本testVec, 调用决策树inputTree，labels是特征的字面意思映射，获取分类结果    
 def classify(inputTree,featLabels,testVec):
-    # 找到树或子树的根节点
+	# firstStr是root feature
     firstStr = inputTree.keys()[0]
-    # 根节点下的子树 dict类型
     secondDict = inputTree[firstStr]
     featIndex = featLabels.index(firstStr)
+	# 获取测试样本里root feature对应的值
     key = testVec[featIndex]
     valueOfFeat = secondDict[key]
-	# 如果不是叶子节点就递归查找
-    if isinstance(valueOfFeat, dict):
+    if isinstance(valueOfFeat, dict): 
         classLabel = classify(valueOfFeat, featLabels, testVec)
-	# 是叶子节点就返回分类结果
     else: classLabel = valueOfFeat
     return classLabel
-
-if __name__ == "__main__":
-    myDat,labels=createDataSet()
-    myTree=createTree(myDat,labels)
-    print("myTree",myTree)
