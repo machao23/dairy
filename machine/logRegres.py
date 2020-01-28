f5e7dee7e3e40827b267da9a2600d0cee55fa325
ma_chao <ma_chao@Ctrip.com>
2020-01-28 15:13:22 +0800

    Stochastic gradient descent

--- machine/logRegres.py
+++ machine/logRegres.py
@@ -77,62 +77,36 @@ def plotBestFit(weights):
     plt.xlabel('X1'); plt.ylabel('X2');
     plt.show()
 
+# 随机梯度上升算法
 def stocGradAscent0(dataMatrix, classLabels):
     m,n = shape(dataMatrix)
     alpha = 0.01
     weights = ones(n)   #initialize to all ones
+	# 随机梯度最大的区别是每次只迭代一个样本，每个样本只迭代一次
+	# 批量梯度迭代一定的次数，每次遍历计算预测所有样本
     for i in range(m):
         h = sigmoid(sum(dataMatrix[i]*weights))
         error = classLabels[i] - h
         weights = weights + alpha * error * dataMatrix[i]
     return weights
 
+# 在全量梯度的精度和随机梯度的性能上的均衡算法
 def stocGradAscent1(dataMatrix, classLabels, numIter=150):
     m,n = shape(dataMatrix)
     weights = ones(n)   #initialize to all ones
     for j in range(numIter):
         dataIndex = range(m)
         for i in range(m):
+			# 步长每次迭代后需要调整减小，缓解数据波动，越到后面，步长越小，避免跨过最优解；
+			# 小步长会让优化问题收敛，大步长会让优化问题发散
+			# 随机梯度算法因为每次只计算一个样本，这个样本随机选择的不一定是符合大部分情况的好样本，前进的方向就会不对，导致回归系数收敛偏差的过程中产生波动
+			# 但是最终的收敛方向和批量梯度算法是一致的，毕竟大部分样本都是好样本
             alpha = 4/(1.0+j+i)+0.0001    #apha decreases with iteration, does not 
             randIndex = int(random.uniform(0,len(dataIndex)))#go to 0 because of the constant
+			# 随机选择一个样本
             h = sigmoid(sum(dataMatrix[randIndex]*weights))
             error = classLabels[randIndex] - h
             weights = weights + alpha * error * dataMatrix[randIndex]
             del(dataIndex[randIndex])
     return weights
-
-def classifyVector(inX, weights):
-    prob = sigmoid(sum(inX*weights))
-    if prob > 0.5: return 1.0
-    else: return 0.0
-
-def colicTest():
-    frTrain = open('horseColicTraining.txt'); frTest = open('horseColicTest.txt')
-    trainingSet = []; trainingLabels = []
-    for line in frTrain.readlines():
-        currLine = line.strip().split('\t')
-        lineArr =[]
-        for i in range(21):
-            lineArr.append(float(currLine[i]))
-        trainingSet.append(lineArr)
-        trainingLabels.append(float(currLine[21]))
-    trainWeights = stocGradAscent1(array(trainingSet), trainingLabels, 1000)
-    errorCount = 0; numTestVec = 0.0
-    for line in frTest.readlines():
-        numTestVec += 1.0
-        currLine = line.strip().split('\t')
-        lineArr =[]
-        for i in range(21):
-            lineArr.append(float(currLine[i]))
-        if int(classifyVector(array(lineArr), trainWeights))!= int(currLine[21]):
-            errorCount += 1
-    errorRate = (float(errorCount)/numTestVec)
-    print "the error rate of this test is: %f" % errorRate
-    return errorRate
-
-def multiTest():
-    numTests = 10; errorSum=0.0
-    for k in range(numTests):
-        errorSum += colicTest()
-    print "after %d iterations the average error rate is: %f" % (numTests, errorSum/float(numTests))
         
