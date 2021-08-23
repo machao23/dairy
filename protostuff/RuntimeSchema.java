5ae150d88f43716b068595ab9470d6ff393c940b
马超 <ma_chao@Ctrip.com>
2021-08-23 11:26:43 +0800

    unsafe

--- protostuff/RuntimeSchema.java
+++ protostuff/RuntimeSchema.java
@@ -15,3 +15,50 @@ public final class RuntimeSchema<T> extends MappedSchema<T> {
         }
     }
 }
+
+// 通过Unsafe方式实例化
+public final class RuntimeUnsafeFieldFactory {
+	public static final RuntimeFieldFactory<String> STRING = new RuntimeFieldFactory<String>(ID_STRING)
+    {
+        public <T> Field<T> create(int number, java.lang.String name, 
+                final java.lang.reflect.Field f, IdStrategy strategy)
+        {
+			// 使用Unsafe对Field取值和赋值，不依赖具体的Getter和Setter
+            final long offset = us.objectFieldOffset(f);
+            return new Field<T>(FieldType.STRING, number, name, 
+                    f.getAnnotation(Tag.class))
+            {                  
+                public void mergeFrom(Input input, T message) throws IOException
+                {
+                    us.putObject(message, offset, input.readString());
+                }
+                public void writeTo(Output output, T message) throws IOException
+                {
+					// Unsafe方式取值
+                    String value = (String)us.getObject(message, offset);
+                    if(value!=null)
+                        output.writeString(number, value, false);
+                }
+                public void transfer(Pipe pipe, Input input, Output output, 
+                        boolean repeated) throws IOException
+                {
+                    input.transferByteRangeTo(output, true, number, repeated);
+                }
+            };
+        }
+    };
+}
+
+public final class ProtostuffOutput extends WriteSession implements Output {
+	// 使用PB方式序列化string类型字段值
+	public void writeString(int fieldNumber, String value, boolean repeated) throws IOException
+    {
+        tail = sink.writeStrUTF8VarDelimited(
+                value, 
+                this, 
+                sink.writeVarInt32(
+                        makeTag(fieldNumber, WIRETYPE_LENGTH_DELIMITED), 
+                        this, 
+                        tail));
+	}
+}
